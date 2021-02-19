d19639a6b838aab4a5744428a6cb260390cce4ea
Рома <roma-11199@mail.ru>
2021-02-19 11:06:53 +0400

    Job

--- IpAddrCounter/src/Main.java
+++ IpAddrCounter/src/Main.java
@@ -6,11 +6,12 @@ public class Main {
         String path = "C:\\Users\\Рома\\Desktop\\ip_addresses";
         int count = 0;
         BufferedReader is = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+        BufferedReader is2 = is;
         try {
             while ((is.readLine()) != null){
                 boolean sort = true;
-                while ((is.readLine()) != null){
-                    if(is.readLine().equals(is.readLine())){
+                while ((is2.readLine()) != null){
+                    if(is.readLine().equals(is2.readLine())){
                         sort = false;
                     }
                     if (sort){
