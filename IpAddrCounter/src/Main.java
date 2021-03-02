2fe02f4890dcbbb701a1a6e3a4656e05221d6d84
Рома <roma-11199@mail.ru>
2021-03-02 21:52:13 +0400

    Job

--- IpAddrCounter/src/Main.java
+++ IpAddrCounter/src/Main.java
@@ -1,30 +1,18 @@
 import java.io.*;
-import java.util.HashSet;
-
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         String path = "C:\\Users\\Рома\\Desktop\\ip_addresses";
-        int count = 0;
-        HashSet hashSet = new HashSet();
         BufferedReader is = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
-        BufferedReader is2 = is;
-        try {
-            while ((is.readLine()) != null){
-                boolean sort = true;
-                while ((is2.readLine()) != null) {
-                    if (is.readLine().equals(is2.readLine())) {
-                        sort = false;
-                        hashSet.add(is.readLine());
-                    }
-                }
-                if (sort){
-                    count++;
-                }
-            }
-            System.out.println(count + hashSet.size());
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
+        Stream<String> line = is.lines();
+        //line = line.distinct();
+        //LinkedHashSet<String> set = new LinkedHashSet<>();
+        //line.forEach(x -> set.add(x));
+        System.out.println(line);
+    }
+
+    public boolean equals(Object obj){
+        return (this == obj);
     }
 }
